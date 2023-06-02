7d6c3936bef7ba64e049ce6e033dbcdeb3af0d97
ChanCode <126632248+ChanCode-Sample@users.noreply.github.com>
2023-06-02 11:12:59 +0900

    Add files via upload

--- service-worker-sample/update-sample/sw.js
+++ service-worker-sample/update-sample/sw.js
@@ -1,9 +1,9 @@
 // Ver1
-self.addEventListener('install', () => console.log('install Ver1'));
-self.addEventListener('activate', () => console.log('activate Ver1'));
+// self.addEventListener('install', () => console.log('install Ver1'));
+// self.addEventListener('activate', () => console.log('activate Ver1'));
 
 // Ver2　： Ver1をコメントアウトしてVer2を有効化し、更新を確認
-// self.addEventListener('install', () => console.log('install Ver2'));
-// self.addEventListener('activate', () => console.log('activate Ver2'));
+self.addEventListener('install', () => console.log('install Ver2'));
+self.addEventListener('activate', () => console.log('activate Ver2'));
 
 
